43940b9cbcec8b3c7f561276695e011036843a10
René Moser <moserre@max.moser.lan>
2009-09-13 12:33:25 +0200

    Added ini parser func

--- git-ftp.sh
+++ git-ftp.sh
@@ -42,6 +42,22 @@ releaseLock() {
     rm -f "${LCK_FILE}"
 }
 
+# Copy 'n' pasted ini parser func 
+# from http://ajdiaz.wordpress.com/2008/02/09/bash-ini-parser/
+cfg.parser () {
+    IFS=$'\n' && ini=( $(<$1) )              # convert to line-array
+    ini=( ${ini[*]//;*/} )                   # remove comments
+    ini=( ${ini[*]/#[/\}$'\n'cfg.section.} ) # set section prefix
+    ini=( ${ini[*]/%]/ \(} )                 # convert text2function (1)
+    ini=( ${ini[*]/=/=\( } )                 # convert item to array
+    ini=( ${ini[*]/%/ \)} )                  # close array parenthesis
+    ini=( ${ini[*]/%\( \)/\(\) \{} )         # convert text2function (2)
+    ini=( ${ini[*]/%\} \)/\}} )              # remove extra parenthesis
+    ini[0]=''                                # remove first element
+    ini[${#ini[*]} + 1]='}'                  # add the last brace
+    eval "$(echo "${ini[*]}")"               # eval the result
+}
+
 # Checks locking, make sure this only run once a time
 if [ -f "${LCK_FILE}" ]; then
 
