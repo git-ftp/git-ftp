6d430a7b93771885cb87771309abacb21e8702ba
René Moser <mail@renemoser.net>
2012-06-22 14:02:44 +0200

    Added duration output in seconds

--- tests/git-ftp-test.sh
+++ tests/git-ftp-test.sh
@@ -14,9 +14,13 @@ oneTimeSetUp() {
 
 	echo Starting FTP Server
 	sudo /opt/lampp/lampp start > /dev/null 2>&1
+	START=$(date +%s)
 }
 
 oneTimeTearDown() {
+	END=$(date +%s)
+	DIFF=$(( $END - $START ))
+	echo "It took $DIFF seconds"
 	echo Stopping FTP Server
 	sudo /opt/lampp/lampp stop > /dev/null 2>&1
 }
